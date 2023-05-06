d4a34cefab736ef074d53aac65e626dd2d7d889b
S-T-R-A-H <132746765+S-T-R-A-H@users.noreply.github.com>
2023-05-06 12:15:37 +0300

    Update NewToken.sol

--- NewToken.sol
+++ NewToken.sol
@@ -4,10 +4,10 @@ import "./ERC20Standard.sol";
 
 contract NewToken is ERC20Standard {
 	constructor() public {
-		totalSupply = 123;
-		name = "Best ever coin";
-		decimals = 4;
-		symbol = "BEC";
+		totalSupply = 10;
+		name = "10 Coin";
+		decimals = 5;
+		symbol = "10C";
 		version = "1.0";
 		balances[msg.sender] = totalSupply;
 	}
